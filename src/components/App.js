c3e949d96465ef3ea9e1f06d57f51c8562c61e2a
Sirawit Techavanitch <sirawite@ais.co.th>
2021-01-20 17:51:29 +0700

    remove module doesn't need

--- src/components/App.js
+++ src/components/App.js
@@ -1,57 +1,17 @@
 import React, { Component } from 'react';
-import Web3 from 'web3';
 import './App.css';
-import Meme from '../abis/Meme.json'
 
 const ipfsClient = require('ipfs-http-client')
 const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' }) // leaving out the arguments will default to these values
 
 class App extends Component {
 
-  async componentWillMount() {
-    await this.loadWeb3()
-    await this.loadBlockchainData()
-  }
-
-  async loadWeb3() {
-    if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum)
-      await window.ethereum.enable()
-    }
-    else if (window.web3) {
-      window.web3 = new Web3(window.web3.currentProvider)
-    }
-    else {
-      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
-    }
-  }
-
-  async loadBlockchainData() {
-    const web3 = window.web3
-    // Load account
-    const accounts = await web3.eth.getAccounts()
-    this.setState({ account: accounts[0] })
-    const networkId = await web3.eth.net.getId()
-    const networkData = Meme.networks[networkId]
-    if(networkData) {
-      const contract = web3.eth.Contract(Meme.abi, networkData.address)
-      this.setState({ contract })
-      const memeHash = await contract.methods.get().call()
-      this.setState({ memeHash })
-    } else {
-      window.alert('Smart contract not deployed to detected network.')
-    }
-  }
-
   constructor(props) {
     super(props)
 
     this.state = {
-      memeHash: '',
-      contract: null,
-      web3: null,
+      memeHash: 'QmUCTkyFkg6ScFeHzNwYDSoUnCYtejiJZdZQtXw5RtU5bW',
       buffer: null,
-      account: null
     }
   }
 
@@ -75,9 +35,7 @@ class App extends Component {
         console.error(error)
         return
       }
-       this.state.contract.methods.set(result[0].hash).send({ from: this.state.account }).then((r) => {
-         return this.setState({ memeHash: result[0].hash })
-       })
+      this.setState({ memeHash: result[0].hash })
     })
   }
 
@@ -91,22 +49,24 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Meme of the Day
+            Sample IPFS
           </a>
         </nav>
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
+                <img width="800" height="600" src={`https://ipfs.infura.io/ipfs/${this.state.memeHash}`} />
+                <p>&nbsp;</p>
                 <a
-                  href="http://www.dappuniversity.com/bootcamp"
+                  className="navbar-brand col-sm-3 col-md-2 mr-0"
+                  href={`https://ipfs.infura.io/ipfs/${this.state.memeHash}`}
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <img src={`https://ipfs.infura.io/ipfs/${this.state.memeHash}`} />
+                  View on IPFS gateway
                 </a>
-                <p>&nbsp;</p>
-                <h2>Change Meme</h2>
+                <h2>Change Picture</h2>
                 <form onSubmit={this.onSubmit} >
                   <input type='file' onChange={this.captureFile} />
                   <input type='submit' />
